b783be3fc97b3d59cd1629a755f6b02f8ba11b6c
Amjed Mrissa <mrissamjed@gmail.com>
2023-11-30 01:16:18 +0100

    changes

--- page2.js
+++ page2.js
@@ -1,21 +1,40 @@
 
 
   function makeGover(name,weather,location,images){
-    var gover={
-      name:name,
-      weather:weather,
-      location:location,
-      images:images,
+    var gover={}
+      gover.name=name
+      gover.weather=weather
+      gover.location=location
+      gover.list=[]
+      // gover.add=addGover
+      // gover.remove=removeGover  
+      // gover.sort=sortGover
+      return gover
     }
-    return gover
-}
-  var gover1 =makeGover("Hammamet","Sunny","Cap Bon",["https://a.cdn-hotels.com/gdcs/production146/d1430/e1a0723c-a2f6-491f-b099-8fb1bee294b2.jpg?impolicy=fcrop&w=800&h=533&q=medium","https://a.cdn-hotels.com/gdcs/production199/d1976/a073366a-43a5-4707-8321-488349ce7c9e.jpg?impolicy=fcrop&w=1600&h=1066&q=medium"])
-  var gover2 =makeGover("Jerba","Cloudy","South",[,,,])
+    
+  var gover1 =makeGover("Hammamet","Sunny","Cap Bon")
+ 
+  var gover2 =makeGover("Jerba","Cloudy","South")
   var gover3 =makeGover("Sousse","Sunny","South",[,,,])
 
+console.log(this.list)
   $('p1').text(gover1.name) 
+ 
   $('p1').append(gover1.weather)
+  
   $('p1').append(gover1.location)
+
+  $('p2').text(gover2.name) 
+ 
+  $('p2').append(gover2.weather)
+  
+  $('p2').append(gover2.location)
+  $('p3').text(gover3.name) 
+ 
+  $('p3').append(gover3.weather)
+  
+  $('p3').append(gover3.location)
+  
 
 
 
@@ -24,12 +43,31 @@
     
 
   $(document).ready(function(){
-    $("button").click(function(){
+    ;$(".image1").click(function(){
       $(".Hammamet").toggleClass("active");
-    });
+    })
+    ;$(".image2").click(function(){
+      $(".jerba").toggleClass("active");
+    })
+    ;$(".image3").click(function(){
+      $(".Sousse").toggleClass("active");
+    })
+    // ;$(".image4").click(function(){
+    //   $(".Hammamet").toggleClass("active");
+    // })
+    // ;$(".image5").click(function(){
+    //   $(".Hammamet").toggleClass("active")
+    // })
+
+
+
+
+
+
     $(".Dje").click(function(){
         $(".jerba").toggleClass("active");
       });
+    
       $(".click").click(function(){
         $(".Sousse").toggleClass("active");
       });
